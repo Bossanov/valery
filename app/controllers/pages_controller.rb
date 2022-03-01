5b706cb167294d8149e4482c62715c1bd6d000bd
Bossanov <31960270+Bossanov@users.noreply.github.com>
2022-03-01 21:09:53 +0100

    yes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,19 @@ class PagesController < ApplicationController
   def home
   end
 
+  def cabinet
+  end
+
+  def conseils
+  end
+
+  def contact
+  end
+
+  def drgrard
+  end
+
+  def services
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
+  get 'pages/cabinet'
+  get 'pages/conseils'
+  get 'pages/contact'
+  get 'pages/drgrard'
+  get 'pages/services'
 end
